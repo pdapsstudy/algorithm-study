4b65d7df9b883d79f44a35ec647bb3476770e897
sdc05103 <heeni1004@gmail.com>
2024-04-11 18:47:52 +0900

    3rd_주석추가

--- week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
+++ week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-words = set()
+words = set()   # 리스트 쓰면 제대로 안 지워져 왜지..?
 for _ in range(N):
     word = input().rstrip()
     words.add(word)
@@ -11,6 +11,6 @@ for _ in range(N):
 for _ in range(M):
     used = set(input().rstrip().split(','))
     for i in used:
-        words.discard(i)
+        words.discard(i)    # remove()로 하면 오류 체크 필요,,
     
     print(len(words))
