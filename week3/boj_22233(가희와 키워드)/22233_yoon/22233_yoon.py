c0baa65c9d98997ed79f7ac29a7c1530f2fa723f
sdc05103 <heeni1004@gmail.com>
2024-04-11 18:52:59 +0900

    런타임 에러 추가

--- week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
+++ week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-words = set()   # 리스트 쓰면 제대로 안 지워져 왜지..?
+words = set()   
 for _ in range(N):
     word = input().rstrip()
     words.add(word)
@@ -11,6 +11,27 @@ for _ in range(N):
 for _ in range(M):
     used = set(input().rstrip().split(','))
     for i in used:
-        words.discard(i)    # remove()로 하면 오류 체크 필요,,
+        words.discard(i)    # remove()로 하면 오류 체크 필요..
     
     print(len(words))
+
+# 런타임 에러 코드
+# import sys
+# input = sys.stdin.readline
+
+# N, M = map(int, input().split())
+
+# words = []
+
+# for _ in range(N):
+#     word = input().rstrip()
+#     words.append(word)
+
+# for _ in range(M):
+#     a, b = input().rstrip().split(',')
+#     if a in words:
+#         words.remove(a)
+#     if b in words:
+#         words.remove(b)
+#     print(len(words))
+    
