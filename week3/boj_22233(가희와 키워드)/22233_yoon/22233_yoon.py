081ddcf4ff71447306f8f65355f4208e85a490cb
sdc05103 <heeni1004@gmail.com>
2024-04-11 18:46:22 +0900

    2nd_answer

--- week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
+++ week3/boj_22233(가희와 키워드)/22233_yoon/22233_yoon.py
@@ -3,7 +3,10 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-words = {input().rstrip() for _ in range(N)}
+words = set()
+for _ in range(N):
+    word = input().rstrip()
+    words.add(word)
 
 for _ in range(M):
     used = set(input().rstrip().split(','))
