8b349be3e5ee6fe38b6832cd92f8c4e33f3b7cdc
yoonjaeuk <jasper09@ajou.ac.kr>
2024-04-11 20:02:40 +0900

    22233_uk

--- week3/boj_22233(가희와 키워드)/22233_uk/22233.py
+++ week3/boj_22233(가희와 키워드)/22233_uk/22233.py
@@ -1,22 +1,17 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000000)
 
 if __name__ == "__main__":
-    #print("입력하세요") 
-    tmp = {}
-    tmp2 = {}
-    cnt = 0
-    cnt2 = 0
-    N, M = map(int, input().split())  #N = 메모장에 적은 키워드 M = 블로그에 쓴 글의 개수
+    print("입력하세요") 
+    N, M = map(int, input().split())
+    tmp = dict()
     for _ in range(N):
-        tmp[str(input().rstrip())] = 1
-        cnt += 1
-    for i in range(M):
-        tmp2 = list(str(input().rstrip().split(',')))     
-        for j in tmp2:
-            if i in tmp.keys():
-                if tmp[i] == 1:
-                    tmp[i] = 0
-                    cnt -= 1
+        tmp[input().rstrip()] = 1
+    print(tmp)    
+    cnt = N
+    for _ in range(M):
+        entries = input().rstrip().split(',')
+        print(entries)
+        for i in entries:
+            if i in tmp and tmp[i] == 1:
+                tmp[i] -= 1
+                cnt -= 1
         print(cnt)
