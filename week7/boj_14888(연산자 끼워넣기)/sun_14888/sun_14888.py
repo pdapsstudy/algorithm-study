80b0394920e92b66e0f7b8f29e4370f754e11ec8
Phoebe125 <ted9988sm9988@gmail.com>
2024-05-29 19:35:11 +0900

    sun: 14888번 코드 추가 - 순열 사용

--- week7/boj_14888(연산자 끼워넣기)/sun_14888/sun_14888.py
+++ week7/boj_14888(연산자 끼워넣기)/sun_14888/sun_14888.py
@@ -1,5 +1,7 @@
 import sys
-from itertools import 
+from itertools import permutations
+import copy
+
 input = sys.stdin.readline
 
 N = int(input())
@@ -9,34 +11,41 @@ max_output = - sys.maxsize
 min_output = sys.maxsize
 
 
-def calculate(cal_item, num_list):
-    num1 = num_list.pop(0)
-    num2 = num_list.pop(1)
-
-    if cal_item == 0:
+def calculate(cal_item, num1, num2):
+    if cal_item == "+":
         new = num1 + num2
-    elif cal_item == 1:
+    elif cal_item == "-":
         new = num1 - num2
-    elif cal_item == 2:
+    elif cal_item == "*":
         new = num1 * num2
     else:
-        new = num1 // num2
-    num_list.insert(0, new)
-    return num_list
+        if num1 > 0:
+            new = (num1 // num2)
+        else:
+            new = (-num1 // num2) * -1
+    return new
 
 
-def process(output, cal_list):
-    global max_output, min_output
+def process():
+    global max_output, min_output, numbers
 
-    if len(output) == 1:
-        num = output[0]
-        if num > max_output:
-            max_output = num
-        if num < min_output:
-            min_output = num
-        return
+    # 전체 가능한 모든 연산자 조합
+    cal_list = ["+"] * cal[0] + ["-"] * \
+        cal[1] + ["*"] * cal[2] + ["/"] * cal[3]
 
-    for i in range(5):
-        output = calculate(i, output)
-        process(cal_list)
+    for j in permutations(cal_list, N-1):
+        idx = 1
+        output = numbers[0]
+        for cal_item in j:
+            output = calculate(cal_item, output, numbers[idx])
+            idx += 1
+        # 최대 / 최소 계산하기
+        if output > max_output:
+            max_output = output
+        if output < min_output:
+            min_output = output
 
+
+process()
+print(max_output)
+print(min_output)
